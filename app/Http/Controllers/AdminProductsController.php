28782763766a2ea0598872c9688a95b2141702e9
RazvanCristianDrelciuc <114404515+RazvanCristianDrelciuc@users.noreply.github.com>
2022-11-17 14:17:19 +0200

    fix

--- app/Http/Controllers/AdminProductsController.php
+++ app/Http/Controllers/AdminProductsController.php
@@ -5,15 +5,14 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
 
 class AdminProductsController extends Controller
 {
     public function index(Request $request)
     {
         $products = Product::all();
-        if ($request->expectsJson()) {
-            return response($products);
-        }
         return view('products.index', [
             'products' => $products,
         ]);
@@ -21,9 +20,6 @@ class AdminProductsController extends Controller
 
     public function create(Request $request)
     {
-        if ($request->expectsJson()) {
-            return response()->json(['message' => 'has been created with succes']);
-        }
         return view('product');
     }
 
@@ -35,37 +31,42 @@ class AdminProductsController extends Controller
             'title' => 'required',
             'description' => 'required',
             'price' => 'required',
-
+            'image' => 'image|mimes:jpeg,png,jpg|max:10240',
         ]);
+
+        if (file_exists(public_path('/storage/images/' . $product->image))) {
+            unlink(public_path('/storage/images/' . $product->image));
+        } else {
+            dd('File does not exists.');
+        }
 
         $product->fill(['title' => $request->input('title'),
             'description' => $request->input('description'),
             'price' => $request->input('price'),
-            'image'=>$this->uploadImage($request->image),
+            'image' => $this->uploadImage($request->image),
         ]);
         $product->update();
-
-        if ($request->expectsJson()) {
-            return response()->json(['message' => 'has been updated with succes']);
-        }
 
         return redirect()->route('products.index');
     }
 
     public function destroy($id, Request $request)
     {
-        $productRemove = Product::find($id);
-        $productRemove->delete();
+        $product = Product::findOrFail($id);
+
+        if (file_exists(public_path('/storage/images/' . $product->image))) {
+            unlink(public_path('/storage/images/' . $product->image));
+        } else {
+            allert('File does not exists.');
+            return redirect()->route('products.index');
+        }
+        $product->delete();
         if (session()->exists('cart')) {
             foreach (session('cart') as $key => $val) {
                 if ($val['product_id'] == $id) {
                     session()->pull('cart.' . $key);
                 }
             }
-        }
-
-        if ($request->expectsJson()) {
-            return response()->json(['message' => 'has been deleted']);
         }
 
         return redirect()->route('products.index');
@@ -77,24 +78,19 @@ class AdminProductsController extends Controller
             'title' => 'required',
             'description' => 'required',
             'price' => 'required',
+            'image' => 'image|mimes:jpeg,png,jpg|max:10240',
         ]);
 
         $product = new Product;
         $product->fill(['title' => $request->input('title'),
             'description' => $request->input('description'),
             'price' => $request->input('price'),
-        //    'image' => $request->input('image')
-            'image'=>$this->uploadImage($request->image)
+            'image' => $this->uploadImage($request->image)
         ]);
         $product->save();
 
         return redirect()->route('products.index');
 
-        if ($request->expectsJson()) {
-            return response()->json(['message' => 'The product has been added with success!']);
-        }
-
-        return redirect()->route('products.index');
     }
 
     function uploadImage($image)
@@ -102,18 +98,12 @@ class AdminProductsController extends Controller
         $fileExt = $image->extension();
         $imageName = hash('sha1', $image);
         $image->move(storage_path('app/public/images/'), $imageName . '.' . $fileExt);
-
         return $imageName . '.' . $fileExt;
     }
 
     public function edit($id, Request $request)
     {
-
-        $product = Product::find($id);
-
-        if ($request->expectsJson()) {
-            return response($product);
-        }
+        $product = Product::findOrFail($id);
 
         return view('product', ['product' => $product]);
     }
